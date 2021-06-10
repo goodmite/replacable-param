e833151328466015d6646aba9798cb04125d4127
good mite <sandeepkgupta007@gmail.com>
2021-06-10 21:35:48 +0530

    fix: sravani changes

--- demo.js
+++ demo.js
@@ -1,16 +1,15 @@
-import {MsgBox} from "./chatutils.js";
+import { MsgBox } from "./chatutils.js";
 
-const editor = document.getElementById('wrapper');
+const editor = document.getElementById("wrapper");
 
-const msgBox = MsgBox(editor);
-let template = `Hello, <customer> this is the <text1> <text2> <text3>`
+const msgBox = MsgBox();
+let template = `Hello, <customer> this is the <text1> <text2> <text3>`;
 
 /*format message*/
 const message = msgBox.formatTemplateMessage(template, {});
-editor.querySelector('#editor-froala').innerHTML = message;
+editor.querySelector("#editor-froala").innerHTML = message;
 console.log(message);
 
-document.getElementById('validate').addEventListener('click', () => {
-    alert(msgBox.checkIfParamsReplaced());
-})
-
+document.getElementById("validate").addEventListener("click", () => {
+  alert(msgBox.checkIfParamsReplaced());
+});
